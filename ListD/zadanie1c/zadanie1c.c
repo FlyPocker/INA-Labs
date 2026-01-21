1811b82379514745329f7a9bf0c9ca333927ce13
FlyPocker <michas.rudzik@gmail.com>
2026-01-21 16:04:35 +0100

    Lista dodatkowa

---
+++ ListD/zadanie1c/zadanie1c.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void CalculateChange(int* C, int* U, int* d, int C_size, int d_size) {
+    int d_min, C_min, Temp; // d_min-indeks nominalu dla najmniejszego wyniku C_min-najmnijeszy wynik
+    for (int i = 1; i <= C_size; i++) {
+        C_min = -1;
+        d_min = 0;
+        for (int j = 1; j <= d_size; j++) {
+            if (d[j] <= i) {
+                Temp = C[i - d[j]] + 1;
+                if (Temp != 0 && (Temp < C_min || C_min == -1)) {
+                    C_min = Temp;
+                    d_min = j;
+                }
+            } else {
+                break;
+            }
+        }
+        C[i] = C_min;
+        U[i] = d_min;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int MaxChange, Temp, D_size;
+    FILE *InFile;
+
+    if (argc < 2) {
+        printf("Zla liczba argumentow\n");
+        return -1;
+    }
+
+    InFile = fopen(argv[1], "r");
+    if (InFile == NULL) return -1;
+
+    fscanf(InFile, "%d", &D_size);
+
+    int *Denom = (int*)malloc((D_size + 1) * sizeof(int));
+    int *Denom_Count = (int*)malloc((D_size + 1) * sizeof(int));
+
+    for (int i = 1; i <= D_size; i++) {
+        fscanf(InFile, "%d", &Temp);
+        Denom[i] = Temp;
+        Denom_Count[i] = 0;
+    }
+    fclose(InFile);
+
+    MaxChange = 0;
+    for (int i = 2; i < argc; i++) {
+        if (sscanf(argv[i], "%d", &Temp) != 1) {
+            printf("Zly typ zmiennej\n");
+            return -1;
+        }
+        if (MaxChange < Temp) {
+            MaxChange = Temp;
+        }
+    }
+
+    int *Cost = (int*)malloc((MaxChange + 1) * sizeof(int));
+    int *Used = (int*)malloc((MaxChange + 1) * sizeof(int));
+
+    for (int i = 0; i <= MaxChange; i++) {
+        Cost[i] = -1;
+        Used[i] = 0;
+    }
+    Cost[0] = 0;
+
+    CalculateChange(Cost, Used, Denom, MaxChange, D_size);
+
+    for (int i = 2; i < argc; i++) {
+        sscanf(argv[i], "%d", &Temp);
+        
+        if (Cost[Temp] == -1) {
+            printf("%s ==> No solution!\n", argv[i]);
+        } else {
+            printf("%s ==> %d\n", argv[i], Cost[Temp]);
+            while (Temp != 0) {
+                Denom_Count[Used[Temp]] = Denom_Count[Used[Temp]] + 1;
+                Temp = Temp - Denom[Used[Temp]];
+            }
+            for (int j = 1; j <= D_size; j++) {
+                if (Denom_Count[j] != 0) {
+                    printf(" %d x %d\n", Denom_Count[j], Denom[j]);
+                    Denom_Count[j] = 0;
+                }
+            }
+        }
+    }
+
+    free(Denom_Count);
+    free(Cost);
+    free(Used);
+    free(Denom);
+    return 0;
+}
---
+++ ListD/zadanie1p/zadanie1p.py
@@ -0,0 +1,77 @@
+import sys
+
+def CalculateChange(C, U, d, C_size, d_size):
+    # d_min-indeks nominalu dla najmniejszego wyniku C_min-najmnijeszy wynik
+    for i in range(1, C_size + 1):
+        C_min = -1
+        d_min = 0
+        for j in range(1, d_size + 1):
+            if d[j] <= i:
+                Temp = C[i - d[j]] + 1
+                if Temp != 0 and (Temp < C_min or C_min == -1):
+                    C_min = Temp
+                    d_min = j
+            else:
+                break
+        C[i] = C_min
+        U[i] = d_min
+
+def main():
+    if len(sys.argv) < 2:
+        print("Zla liczba argumentow")
+        return
+
+    try:
+        in_file = open(sys.argv[1], "r")
+        line = in_file.readline()
+        if not line:
+            in_file.close()
+            return
+        D_size = int(line.strip())
+        
+        Denom = [0] * (D_size + 1)
+        Denom_Count = [0] * (D_size + 1)
+        
+        for i in range(1, D_size + 1):
+            line = in_file.readline()
+            if line:
+                Temp = int(line.strip())
+                Denom[i] = Temp
+        in_file.close()
+    except (ValueError, FileNotFoundError):
+        return
+
+    MaxChange = 0
+    for i in range(2, len(sys.argv)):
+        try:
+            Temp = int(sys.argv[i])
+            if MaxChange < Temp:
+                MaxChange = Temp
+        except ValueError:
+            print("Zly typ zmiennej")
+            return
+
+    Cost = [-1] * (MaxChange + 1)
+    Cost[0] = 0
+    Used = [0] * (MaxChange + 1)
+
+    CalculateChange(Cost, Used, Denom, MaxChange, D_size)
+
+    for i in range(2, len(sys.argv)):
+        Temp = int(sys.argv[i])
+
+        if Cost[Temp] == -1:
+            print(f"{sys.argv[i]} ==> No solution!")
+        else:
+            print(f"{sys.argv[i]} ==> {Cost[Temp]}")
+            while Temp != 0:
+                Denom_Count[Used[Temp]] = Denom_Count[Used[Temp]] + 1
+                Temp = Temp - Denom[Used[Temp]]
+            
+            for j in range(1, D_size + 1):
+                if Denom_Count[j] != 0:
+                    print(f" {Denom_Count[j]} x {Denom[j]}")
+                    Denom_Count[j] = 0
+
+if __name__ == "__main__":
+    main()
