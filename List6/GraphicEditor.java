4ed2cca73e27584290a2165e55b9ceb77fc14827
FlyPocker <michas.rudzik@gmail.com>
2026-04-28 23:47:47 +0200

    lista6 wersja 1.0

---
+++ List6/GraphicEditor.java
@@ -0,0 +1,193 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.transform.Rotate;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.ScrollEvent;
+
+/**
+ * Glowna klasa edytora graficznego
+ * 
+ * @author Michal Rudzik
+ * @version 1.0
+ */
+
+public class GraphicEditor extends Application {
+
+    private double startX;
+    private double startY;
+    /**
+     * Enum obslugujacy poszczegolne tryby wybrane przez uzytkownika
+     * RECTANGLE,CIRCLE,EDIT
+     */
+    public enum ToolType {
+        RECTANGLE, CIRCLE, EDIT
+    }
+    private ToolType currentTool = ToolType.RECTANGLE;
+    private Shape currentShape, selectedShape;
+    
+    
+    /**
+     * Metoda obslugujaca okienko i zdazenia
+    */
+    @Override
+    public void start(Stage primaryStage) {
+
+        BorderPane mainPane = new BorderPane();
+        Pane drawingPane = new Pane();
+        drawingPane.setStyle("-fx-background-color: black;");
+        
+        Rectangle clipRect = new Rectangle();
+        // Bindujemy (łączymy na stałe) wymiary maski z wymiarami panelu
+        clipRect.widthProperty().bind(drawingPane.widthProperty());
+        clipRect.heightProperty().bind(drawingPane.heightProperty());
+        // Ustawiamy maskę na nasz panel
+        drawingPane.setClip(clipRect);
+
+        HBox toolBar = new HBox(10);
+        toolBar.setStyle("-fx-padding: 10px; -fx-background-color: #404040;");
+
+        Label currentToolLabel = new Label("Aktywne narzedzie: "+ currentTool.name());
+        currentToolLabel.setTextFill(Color.WHITE);
+        Button btnRect = new Button("Prostokąt");
+        btnRect.setOnAction(e -> changeTool(ToolType.RECTANGLE, currentToolLabel));
+        Button btnCirc = new Button("Okrąg");
+        btnCirc.setOnAction(e -> changeTool(ToolType.CIRCLE, currentToolLabel));
+        Button btnEdit = new Button("Edycja");
+        btnEdit.setOnAction(e -> changeTool(ToolType.EDIT, currentToolLabel));
+
+        toolBar.getChildren().addAll(btnRect, btnCirc, btnEdit, currentToolLabel);
+
+        // 1. Zaczynamy rysować
+        drawingPane.setOnMousePressed(e -> {
+            startX = e.getX();
+            startY = e.getY();
+            
+            if (e.getButton() == MouseButton.PRIMARY){
+            switch (currentTool){
+                case RECTANGLE:
+                    currentShape = new Rectangle(startX, startY, 0, 0);
+                    break;
+                case CIRCLE:
+                    currentShape = new Circle(startX, startY, 0);
+                    break;
+                case EDIT:
+                    if (selectedShape != null){
+                        selectedShape.setStroke(Color.WHITE);
+                        selectedShape = null;
+                    }
+                    if (e.getTarget() instanceof Shape){
+                        selectedShape = (Shape) e.getTarget();
+                        selectedShape.setStroke(Color.RED);
+                    }
+            }}
+            if (currentShape != null){
+                currentShape.setFill(Color.TRANSPARENT);
+                currentShape.setStroke(Color.WHITE); 
+                drawingPane.getChildren().add(currentShape);
+            }
+
+            
+        });
+
+        // 2. Rozciągamy figurę
+        drawingPane.setOnMouseDragged(e -> {
+            if (currentShape == null & currentTool != ToolType.EDIT) return;
+
+            double currentX = e.getX();
+            double currentY = e.getY();
+
+            switch (currentTool) {
+                case RECTANGLE:
+                    Rectangle rect = (Rectangle) currentShape;
+                    rect.setWidth(Math.abs(currentX-startX));
+                    rect.setHeight(Math.abs(currentY-startY));
+                    rect.setX(Math.min(currentX,startX));
+                    rect.setY(Math.min(currentY,startY));
+                    break;
+                case CIRCLE:
+                    Circle circ = (Circle) currentShape;
+                    circ.setRadius(Math.sqrt(Math.pow(currentX-startX, 2)+Math.pow(currentY-startY, 2)));
+                    break;
+                case EDIT:
+                    if(selectedShape != null){
+                        double offsetX = currentX-startX;
+                        double offsetY = currentX-startY;
+                    if(e.isPrimaryButtonDown()){
+                        offsetX = currentX-startX;
+                        offsetY = currentY-startY;
+                        selectedShape.setTranslateX(selectedShape.getTranslateX()+offsetX);
+                        selectedShape.setTranslateY(selectedShape.getTranslateY()+offsetY);
+                        startX = currentX;
+                        startY = currentY;
+                    }else if(e.isSecondaryButtonDown()){
+                        offsetX = currentX-startX;
+                        double newAngle = selectedShape.getRotate()+offsetX;
+
+                        if(e.isShiftDown()){
+                            double perfectAngle = Math.round(newAngle/45.0)*45.0;
+                            if(Math.abs(newAngle-perfectAngle)<5.0) newAngle = perfectAngle;
+                        }
+                        selectedShape.setRotate(newAngle);
+                        startX = currentX;
+                    }
+                        
+                    }
+                    break;
+            
+            }
+        });
+
+        drawingPane.setOnScroll(e -> {
+            if(currentTool != ToolType.EDIT || selectedShape == null) return;
+            double delta = e.getDeltaY() + e.getDeltaX();
+            double scrollFactor = 1.1;
+            double newScale = selectedShape.getScaleX();
+            if(e.isShiftDown()){
+                scrollFactor = 1.05;
+            }
+            if(delta > 0){
+                newScale = newScale * scrollFactor;
+            }else{
+                newScale = newScale / scrollFactor;
+            }
+            if(newScale > 0.1){
+                selectedShape.setScaleX(newScale);
+                selectedShape.setScaleY(newScale);
+            }
+        });
+
+        // 3. Kończymy rysowanie
+        drawingPane.setOnMouseReleased(e -> {
+            currentShape = null;
+        });
+
+
+        mainPane.setTop(toolBar);
+        mainPane.setCenter(drawingPane);
+
+        Scene scene = new Scene(mainPane, 800, 600);
+        primaryStage.setTitle("Edytor Figur");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void changeTool(ToolType newTool, Label label){
+        currentTool = newTool;
+        label.setText("Aktywne narzedzie: " + currentTool.name());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
